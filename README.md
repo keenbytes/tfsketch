403445b263d2eeed8631669620eda4ec35b81f9a
Mikolaj Gasior <m@gasior.dev>
2025-05-18 01:50:38 +0200

    Add initial code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tfsketch
+
+[![Go Reference](https://pkg.go.dev/badge/gopkg.pl/keenbytes/tfsketch.svg)](https://pkg.go.dev/gopkg.pl/keenbytes/tfsketch) [![Go Report Card](https://goreportcard.com/badge/gopkg.pl/keenbytes/tfsketch)](https://goreportcard.com/report/gopkg.pl/keenbytes/tfsketch)
+
+![tfsketch](tfsketch.png "tfsketch")
+
+A tool for generating clean, minimal SVG diagrams from Terraform modules.  
+It’s still under active development — new version and documentation are coming soon.
+
+## Building
+Run `go build -o tfsketch` to compile the binary.
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"gopkg.pl/mikogs/broccli/v3"
+)
+
+func main() {
+	cli := broccli.NewBroccli("tfdiagram", "Generate SVG from terraform files", "Mikolaj Gasior <m@gasior.dev>")
+
+	cmd := cli.Command("module", "Generate an SVG diagram from a terraform module", drawHandler)
+	cmd.Arg("module-dir", "MODULE_DIR", "Path to terraform module", broccli.TypePathFile, broccli.IsDirectory|broccli.IsExistent|broccli.IsRequired)
+	cmd.Arg("output-file", "OUTPUT_FILE", "Path to output SVG file", broccli.TypePathFile, broccli.IsRequired)
+
+	_ = cli.Command("version", "Prints version", versionHandler)
+	if len(os.Args) == 2 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		os.Args = []string{"App", "version"}
+	}
+
+	os.Exit(cli.Run(context.Background()))
+}
+
+func drawHandler(ctx context.Context, c *broccli.Broccli) int {
+	moduleDir := c.Arg("module-dir")
+	outputFile := c.Arg("output-file")
+
+	module, err := parseTerraformModule(moduleDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse module: %v\n", err)
+		return 1
+	}
+
+	svg := generateSvg(module)
+	os.WriteFile(outputFile, []byte(svg), 0644)
+
+	return 0
+}
+
+func versionHandler(ctx context.Context, c *broccli.Broccli) int {
+	fmt.Fprintf(os.Stdout, VERSION+"\n")
+	return 0
+}
