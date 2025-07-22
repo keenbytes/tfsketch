44aa3ee4b87820198a0eba3209ecd5d1bfa419e3
Mikołaj Gąsior <m@gasior.dev>
2025-07-22 22:43:30 +0200

    Add initial parsing of modules

---
+++ examples/local-modules/terraform/account/modules/module-1/mod2.tf
@@ -0,0 +1,16 @@
+resource "resource_type" "resource_name_mod2_1" {
+  name = "name-mod2-1"
+  key1 = "value1"
+  key2 = "value2"
+}
+
+resource "another_resource" "resource_name_mod2" {
+  key1 = "value1"
+}
+
+resource "resource_type" "resource_name_mod2_2" {
+  name = "name-mod2-2"
+  key1 = "value1_2"
+  key2 = "value2_2"
+}
+
--- traverse.go
+++ traverse.go
@@ -12,6 +12,7 @@ import (
 	hcl "github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/zclconf/go-cty/cty"
 )
 
 var (
@@ -22,6 +23,7 @@ type Directory struct {
 	FullPath    string
 	DisplayPath string
 	Resources   map[string]*Resource
+	Modules map[string]*Directory
 }
 
 type Resource struct {
@@ -33,31 +35,42 @@ type Resource struct {
 
 func traverseTerraformDirectory(root string, resourceType string) error {
 	dirs := make(map[string]*Directory)
+	dirsModules := make(map[string]*Directory)
 
 	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return fmt.Errorf("%w: %w", errTfDirWalk, err)
 		}
 
+		isModulesDir := false
+
 		// Skip directories that contain "modules" in their path
 		relPath, _ := filepath.Rel(root, path)
 		pathParts := strings.Split(filepath.ToSlash(relPath), "/")
 		for _, part := range pathParts {
 			if part == "modules" {
-				if d.IsDir() {
-					return fs.SkipDir // Skip entire directory
-				}
-				return nil // Skip file
+				isModulesDir = true
+				break
 			}
 		}
 
 		if !d.IsDir() && strings.HasSuffix(d.Name(), ".tf") {
 			dir := filepath.Dir(path)
 			dirWithoutRoot := strings.Replace(dir, root, "", 1)
-			dirs[dir] = &Directory{
-				FullPath:    dir,
-				DisplayPath: dirWithoutRoot,
-				Resources:   map[string]*Resource{},
+
+			if isModulesDir {
+				dirsModules[dir] = &Directory{
+					FullPath:    dir,
+					DisplayPath: dirWithoutRoot,
+					Resources:   map[string]*Resource{},
+				}
+			} else {
+				dirs[dir] = &Directory{
+					FullPath:    dir,
+					DisplayPath: dirWithoutRoot,
+					Resources:   map[string]*Resource{},
+					Modules: map[string]*Directory{},
+				}
 			}
 		}
 		return nil
@@ -68,10 +81,51 @@ func traverseTerraformDirectory(root string, resourceType string) error {
 	}
 
 	processDirs(dirs, resourceType)
+	processDirs(dirsModules, resourceType)
+	processModulesInDirs(dirs, dirsModules)
 
 	genMermaid(dirs)
 
 	return nil
+}
+
+func processModulesInDirs(dirs map[string]*Directory, dirsModules map[string]*Directory) {
+	for _, directory := range dirs {
+		if len(directory.Modules) == 0 {
+			continue
+		}
+
+		moduleKeys := make([]string, len(directory.Modules))
+		for moduleKey, _ := range directory.Modules {
+			moduleKeys = append(moduleKeys, moduleKey)
+		}
+
+		for _, moduleKey := range moduleKeys {
+			// key = ResourceName:Path
+			moduleKeyValues := strings.Split(moduleKey, ":")
+			if len(moduleKeyValues) != 2 {
+				continue
+			}
+
+			modulePath := moduleKeyValues[1]
+
+			if !strings.HasPrefix(modulePath, "./modules") {
+				continue
+			}
+
+			modulePathTrimmed := strings.Replace(modulePath, "./", "", 1)
+
+			// search for module in existing "modules" dirs
+			dirModule, ok := dirsModules[filepath.Join(directory.FullPath, modulePathTrimmed)]
+			if !ok {
+				continue
+			}
+			
+			// we got a reference to a module that is local, meaning in a "modules" subdir
+			// let's assign the Directory object so that we can later iterate over its resources
+			directory.Modules[moduleKey] = dirModule
+		}
+	}
 }
 
 func processDirs(dirs map[string]*Directory, resourceTypeToMatch string) {
@@ -94,25 +148,31 @@ func processDirs(dirs map[string]*Directory, resourceTypeToMatch string) {
 			content, _, _ := hclFile.Body.PartialContent(&hcl.BodySchema{
 				Blocks: []hcl.BlockHeaderSchema{
 					{Type: "resource", LabelNames: []string{"kind", "name"}},
+					{Type: "module", LabelNames: []string{"name"}},
 				},
 			})
 
 			for _, block := range content.Blocks {
-				if len(block.Labels) != 2 || block.Type != "resource" {
-					continue
-				}
-				resourceType := block.Labels[0]
-				resourceName := block.Labels[1]
-				if resourceType == resourceTypeToMatch {
-					nameField, _ := getResourceNameField(block)
-					// todo: handle error
+				if len(block.Labels) == 2 && block.Type == "resource" {
+					resourceType := block.Labels[0]
+					resourceName := block.Labels[1]
+					if resourceType == resourceTypeToMatch {
+						nameField, _ := getResourceNameField(block)
+						// todo: handle error
 
-					directory.Resources[resourceName] = &Resource{
-						Type:       resourceType,
-						Name:       resourceName,
-						FieldName:  nameField,
-						TfFileName: file.Name(),
+						directory.Resources[resourceName] = &Resource{
+							Type:       resourceType,
+							Name:       resourceName,
+							FieldName:  nameField,
+							TfFileName: file.Name(),
+						}
 					}
+				}
+
+				if len(block.Labels) == 1 && block.Type == "module" {
+					moduleResourceName := block.Labels[0]
+					sourceField, _ := getSourceField(block)
+					directory.Modules[moduleResourceName+":"+sourceField] = nil
 				}
 			}
 		}
@@ -128,6 +188,14 @@ var (
 			},
 			{
 				Name:     "id",
+				Required: false,
+			},
+		},
+	}
+	TfModule = &hcl.BodySchema{
+		Attributes: []hcl.AttributeSchema{
+			{
+				Name:     "source",
 				Required: false,
 			},
 		},
@@ -179,6 +247,30 @@ func getResourceNameField(block *hcl.Block) (string, error) {
 	return nameField, nil
 }
 
+func getSourceField(block *hcl.Block) (string, error) {
+	name := block.Labels[0]
+
+	bodyContent, _, diags := block.Body.PartialContent(TfModule)
+	if diags.HasErrors() {
+		return "", fmt.Errorf("error parsing block %s.%s: %s", block.Type, name, diags.Error())
+	}
+
+	source := ""
+
+	for attrName, attr := range bodyContent.Attributes {
+		if attrName != "source" {
+			continue
+		}
+
+		value, _ := attr.Expr.Value(nil)
+		if value.Type() == cty.String {
+			source = value.AsString()
+		}
+	}
+
+	return source, nil
+}
+
 func genMermaid(dirs map[string]*Directory) {
 	mermaidDiagram := &strings.Builder{}
 
@@ -200,7 +292,45 @@ flowchart LR
 			elementResourceName := elementPathName + "_" + clearString(resource.Name)
 			elementResourceFieldName := elementResourceName + "_FieldName"
 
-			_, _ = mermaidDiagram.WriteString(fmt.Sprintf("  %s[\"%s\"]:::tf-path --> %s[\"%s\"]:::tf-resource-name --> %s[\"%s\"]:::aws-resource\n", elementPathName, dir.DisplayPath, elementResourceName, resource.Name, elementResourceFieldName, resource.FieldName))
+			_, _ = mermaidDiagram.WriteString(
+				fmt.Sprintf(
+					"  %s[\"%s\"]:::tf-path --> %s[\"%s\"]:::tf-resource-name --> %s[\"%s\"]:::aws-resource\n",
+					elementPathName,
+					dir.DisplayPath,
+					elementResourceName,
+					resource.Name,
+					elementResourceFieldName,
+					resource.FieldName,
+				),
+			)
+		}
+
+		for moduleKey, dirModule := range dir.Modules {
+			if dirModule == nil {
+				continue
+			}
+			moduleElementPathName := strings.ReplaceAll(dirModule.DisplayPath, "/", "_")
+			moduleElementPathName = clearString(elementPathName)
+
+			moduleKeyValues := strings.Split(moduleKey, ":")
+			moduleResourceName := moduleKeyValues[0]
+
+			for _, resource := range dirModule.Resources {
+				elementResourceName := elementPathName + "_mod_" + moduleElementPathName + "_" + clearString(moduleResourceName) + "_" + clearString(resource.Name)
+				elementResourceFieldName := elementResourceName + "_FieldName"
+
+				_, _ = mermaidDiagram.WriteString(
+					fmt.Sprintf(
+						"  %s[\"%s\"]:::tf-path --> %s[\"%s\"]:::tf-resource-name --> %s[\"%s\"]:::aws-resource\n",
+						elementPathName,
+						dir.DisplayPath,
+						elementResourceName,
+						"mod." + moduleResourceName + "." + resource.Name,
+						elementResourceFieldName,
+						resource.FieldName,
+					),
+				)
+			}
 		}
 	}
 
